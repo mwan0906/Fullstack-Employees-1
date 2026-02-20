7d8c62f3b968d86fad62b15f7e11d5b69fe7305d
mwan0906 <mwan0906@gmail.com>
2026-02-20 13:59:47 -0500

    Fix

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS employees;
 CREATE TABLE employees (
   id serial PRIMARY KEY,
   name text NOT NULL,
-  birthday date,
-  salary integer
+  birthday date NOT NULL,
+  salary integer NOT NULL
 );
